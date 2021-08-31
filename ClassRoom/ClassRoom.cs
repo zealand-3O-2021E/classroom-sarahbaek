6330e7f008389334a044e9a3ca7b065a23984d43
sarahBæk2021 <sbaek18@gmail.com>
2021-08-31 15:08:04 +0200

    ClassRoom Class

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Collections;
+
+namespace ClassRoom
+{
+    public class ClassRoom
+    {
+        private string _className;
+        private List<Student> _classList;
+        private DateTime _semesterStart;
+
+        //Default constructor
+        public ClassRoom()
+        {
+
+        }
+
+        public string ClassName { get => _className; set => _className = value; }
+        public List<Student> ClassList { get => _classList; set => _classList = value; }
+        public DateTime SemesterStart { get => _semesterStart; set => _semesterStart = value; }
+    }
+}
