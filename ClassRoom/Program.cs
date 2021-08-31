aebbf0e3699eb855e99987781549275853e7c9df
sarahBæk2021 <sbaek18@gmail.com>
2021-08-31 19:16:21 +0200

    Counted each birthday in each season

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -12,6 +12,7 @@ namespace ClassRoom
         {
             Student myStudent = new Student("Sarah", 8, 14);
             Student myStudent2 = new Student("George", 9, 22);
+
             ClassRoom myClassRoom = new ClassRoom();
             myClassRoom.ClassName = "ComputerScience";
             myClassRoom.SemesterStart = DateTime.Today;
@@ -22,20 +23,16 @@ namespace ClassRoom
 
             myClassMates.Add(myStudent);
             myClassMates.Add(myStudent2);
-            Console.WriteLine(myClassMates.Count());
-
-            foreach (var item in myClassMates)
-            {
-                Console.WriteLine(item.Name, item.BirthMonth, item.Birthday);
-            }
+            //Console.WriteLine(myClassMates.Count());
 
 
+            Student.CountBythMonthBySeason(myClassMates);
             //Student.Season(12);
 
 
             //Console.WriteLine(Student.Season(12));
             //Console.WriteLine(Student.Season(6));
-            Console.WriteLine("You were born in {0}", Student.Season(12));
+            //Console.WriteLine("You were born in {0}", Student.Season(12));
 
             Console.ReadLine();
         }
--- ClassRoom/Student.cs
+++ ClassRoom/Student.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassRoom
 {
-   public class Student
+    public class Student
     {
         private string _name;
         private int _birthMonth;
@@ -25,37 +25,87 @@ namespace ClassRoom
 
         public static string Season(int BirthMonth)
         {
-         
+
             string strSeason;
-      
-         
-                if (BirthMonth == 12 || BirthMonth == 1 || BirthMonth == 2)
-                {
-                    strSeason = "Winter";
-                }
-                else if (BirthMonth == 3 || BirthMonth == 4 || BirthMonth == 5)
-                {
-                
-                    strSeason = "Spring";
-                }
-                else if (BirthMonth == 6 || BirthMonth == 7 || BirthMonth == 8)
-                {
-             
-                    strSeason = "Summer";
-                }
-                else if (BirthMonth == 9 || BirthMonth == 10 || BirthMonth == 11)
-                {
-                    strSeason = "Autumn";
-                }
-                else
-                {
-                
-                
-                    strSeason = "Unkown";
-               }
-            
-            
+
+
+            if (BirthMonth == 12 || BirthMonth == 1 || BirthMonth == 2)
+            {
+                strSeason = "Winter";
+            }
+            else if (BirthMonth == 3 || BirthMonth == 4 || BirthMonth == 5)
+            {
+
+                strSeason = "Spring";
+            }
+            else if (BirthMonth == 6 || BirthMonth == 7 || BirthMonth == 8)
+            {
+
+                strSeason = "Summer";
+            }
+            else if (BirthMonth == 9 || BirthMonth == 10 || BirthMonth == 11)
+            {
+                strSeason = "Autumn";
+            }
+            else
+            {
+
+
+                strSeason = "Unkown";
+            }
+
+
             return strSeason;
         }
+
+
+        //public static void MyMethod()
+        //{
+
+        //    throw new NotImplementedException();
+        //}
+        public static void CountBythMonthBySeason(List<Student> inClassMates)
+
+        {
+            int intCountWinter= 0;
+            int intCountSpring = 0;
+            int intCountSummer = 0;
+            int intCountAutumn = 0;
+
+            foreach (var item in inClassMates)
+            {
+                switch (Student.Season(item.BirthMonth))
+                {
+
+                    case "Winter":
+                        intCountWinter = intCountWinter + 1;
+                        break;
+                    case "Spring":
+                        intCountSpring++;
+                        break;
+                    case "Summer":
+                        intCountSummer++;
+
+                        break;
+
+                    case "Autumn":
+                        intCountAutumn++;
+
+                        break;
+
+                    default:
+                        Console.WriteLine("balalala");
+                        break;
+
+                       
+                }
+            }
+            Console.WriteLine("In winter there is {0}" , intCountWinter);
+            Console.WriteLine("In spring there is {0}", intCountSpring);
+            Console.WriteLine("In summer there is {0}", intCountSummer);
+            Console.WriteLine("In autumn there is {0}", intCountAutumn);
+        }
+
+
     }
 }
