928928342c189a884b89a410ea6ee1f60cf7d2c1
sarahBæk2021 <sbaek18@gmail.com>
2021-08-31 15:43:11 +0200

    Added a list of students and printed to the console

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,27 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            Student myStudent = new Student("Sarah", 8, 14);
+            Student myStudent2 = new Student("George", 9, 22);
+            ClassRoom myClassRoom = new ClassRoom();
+            myClassRoom.ClassName = "ComputerScience";
+            myClassRoom.SemesterStart = DateTime.Today;
+
+
+
+            List<Student> myClassMates = new List<Student>();
+
+            myClassMates.Add(myStudent);
+            myClassMates.Add(myStudent2);
+            Console.WriteLine(myClassMates.Count());
+
+            foreach (var item in myClassMates)
+            {
+                Console.WriteLine(item.Name, item.BirthMonth, item.Birthday);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
         }
     }
 }
