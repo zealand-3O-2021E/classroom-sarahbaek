782d911a7590385329098c94962c9df66d652d28
sarahBæk2021 <sbaek18@gmail.com>
2021-08-31 14:56:38 +0200

    Student Class

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+   public class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthday;
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+            this.Name = name;
+            this.BirthMonth = birthMonth;
+            this.Birthday = birthday;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public int BirthMonth { get => _birthMonth; set => _birthMonth = value; }
+        public int Birthday { get => _birthday; set => _birthday = value; }
+    }
+}
